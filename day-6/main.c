56f4b10eca656351bdbe80406f822cb19c9377fd
daniel <developmentleviathan@gmail.com>
2018-12-06 22:57:13 -0500

    6

--- day-6/main.c
+++ day-6/main.c
@@ -9,8 +9,8 @@
 #include <math.h>
 #include <limits.h>
 
-#define MAX_BOARD_X 375
-#define MAX_BOARD_Y 375
+const int MAX_BOARD_X = 375;
+const int MAX_BOARD_Y = 375;
 
 struct Cord
 {
@@ -29,7 +29,163 @@ struct Point
 };
 
 struct Point * get_point(char * line, int input_count);
-int md(int x1, int y1, int x2, int y2);
+int bad_pos(int x, int y, struct Point ** input_points, int input_count);
+void get_distances(struct Point ** input_points, int input_count, int t);
+
+int main()
+{
+    FILE * fp = fopen("input", "r");
+
+    char line[255];
+    int input_count = 0;
+    struct Point ** points = malloc(sizeof(struct Point *));
+
+    int t = 0;
+    int i;
+    int j;
+    int k;
+
+    int min;
+    int min_id;
+
+    while(fgets(line, 255, fp) != NULL)
+    {
+        points[input_count] = get_point(line, input_count);
+        points = realloc(points, (sizeof(struct Point *)) * ++input_count + 1);
+    }
+
+    get_distances(points, input_count, 0);
+
+    int cz = points[0]->d_count;
+
+    int totals[cz];
+    int duplicates[cz];
+    int mins[cz];
+
+    int a[input_count];
+    int bounds[input_count];
+
+    for(i = 0; i < cz; i++)
+    {
+        min = INT_MAX;
+
+        for(j = 0; j < input_count; j++)
+        {
+            for(k = 0; k < input_count; k++)
+            {
+                if( points[j]->distances[i]->dist ==
+                    points[k]->distances[i]->dist &&
+                    points[j]->id != points[k]->id)
+                {
+                    if(points[j]->distances[i]->dist < min)
+                    {
+                        min = points[j]->distances[i]->dist;
+                    }
+                }
+            }
+        }
+
+        duplicates[i] = min;
+    }
+
+    for(i = 0; i < input_count; i++)
+    {
+        a[i] = 1;
+    }
+
+    for(i = 0; i < cz; i++)
+    {
+        min = INT_MAX;
+
+        for(int j = 0; j < input_count; j++)
+        {
+            if(points[j]->distances[i]->dist < min)
+            {
+                min = points[j]->distances[i]->dist;
+                min_id = points[j]->id;
+
+                mins[i] = min;
+
+                if( (points[j]->distances[i]->y == MAX_BOARD_Y - 1) ||
+                    (points[j]->distances[i]->x == 0) ||
+                    (points[j]->distances[i]->y == 0) ||
+                    (points[j]->distances[i]->x == MAX_BOARD_X - 1))
+                {
+                    bounds[min_id] = 1;
+                }
+            }
+        }
+
+        if(min < duplicates[i])
+        {
+            a[min_id]++;
+        }
+    }
+
+    for(int i = 0; i < input_count; i++)
+    {
+        if(bounds[i] != 1 && a[i] > t)
+        {
+            t = a[i];
+        }
+    }
+
+    printf("(A): %i\n", t);
+    get_distances(points, input_count, 1);
+
+    t = 0;
+
+    for(i = 0; i < cz; i++)
+    {
+        t = 0;
+
+        for( j = 0; j < input_count; j++)
+        {
+           t += points[j]->distances[i]->dist;
+        }
+
+        totals[i] = t;
+    }
+
+    t = 0;
+
+    for(i = 0; i < cz; i++)
+    {
+        if(totals[i] < 10000)
+        {
+            t++;
+        }
+    }
+
+    printf("(B): %i\n", t);
+}
+
+struct Point * get_point(char * line, int input_count)
+{
+    int i = 0;
+
+    char * q;
+    char * co[2];
+
+    struct Point * point = malloc(sizeof(struct Point));
+
+    line[strlen(line) - 1] = '\0';
+
+    q = strtok(line, ", ");
+
+    while(q != NULL)
+    {
+        co[i++] = q;
+        q = strtok(NULL, ", ");
+    }
+
+    point->x = atoi(co[0]);
+    point->y = atoi(co[1]);
+    point->id = input_count;
+    point->d_count = 0;
+
+    return point;
+}
 
 int bad_pos(int x, int y, struct Point ** input_points, int input_count)
 {
@@ -80,7 +236,7 @@ void get_distances(struct Point ** input_points, int input_count, int t)
                 dis[ic] = malloc(sizeof(struct Cord));
                 dis[ic]->x = i;
                 dis[ic]->y = j;
-                dis[ic]->dist = md(i, j, dest->x, dest->y);
+                dis[ic]->dist = abs(i - dest->x) + abs(j - dest->y);
                 dis = realloc(dis, (sizeof(struct Cord *)) * ++ic + 1);
             }
         }
@@ -90,166 +246,3 @@ void get_distances(struct Point ** input_points, int input_count, int t)
     }
 }
 
-int main()
-{
-    FILE * fp = fopen("input", "r");
-
-    char line[255];
-    int input_count = 0;
-    struct Point ** points = malloc(sizeof(struct Point *));
-
-    while(fgets(line, 255, fp) != NULL)
-    {
-        points[input_count] = get_point(line, input_count);
-        points = realloc(points, (sizeof(struct Point *)) * ++input_count + 1);
-    }
-
-    get_distances(points, input_count, 0);
-
-    int cz = points[0]->d_count;
-
-    int max_dups[cz];
-
-    for(int i = 0; i < cz; i++)
-    {
-        int max_dup = INT_MAX;
-
-        for(int j = 0; j < input_count; j++)
-        {
-            for(int k = 0; k < input_count; k++)
-            {
-                int a_dist = points[j]->distances[i]->dist;
-                int b_dist = points[k]->distances[i]->dist;
-                int a_id = points[j]->id;
-                int b_id = points[k]->id;
-
-                if(a_dist == b_dist && a_id != b_id)
-                {
-                    if(a_dist < max_dup)
-                    {
-                        max_dup = a_dist;
-                    }
-                }
-            }
-        }
-
-        max_dups[i] = max_dup;
-    }
-
-    int mins[cz];
-
-    int a[input_count];
-    int bb[input_count];
-
-    for(int i = 0; i < input_count; i++)
-    {
-        a[i] = 1;
-    }
-
-    for(int i = 0; i < cz; i++)
-    {
-        int min = INT_MAX;
-        int min_id;
-
-        for(int j = 0; j < input_count; j++)
-        {
-            if(points[j]->distances[i]->dist < min)
-            {
-                min = points[j]->distances[i]->dist;
-                min_id = points[j]->id;
-
-                mins[i] = min;
-
-                int x = points[j]->distances[i]->x;
-                int y = points[j]->distances[i]->y;
-
-                if( (y == MAX_BOARD_Y - 1) ||
-                    (x == 0) || (y == 0) ||
-                    (x == MAX_BOARD_X - 1))
-                {
-                    bb[min_id] = 1;
-                }
-            }
-        }
-
-        if(min < max_dups[i])
-        {
-            a[min_id]++;
-        }
-    }
-
-    int max = 0;
-
-    for(int i = 0; i < input_count; i++)
-    {
-        if(bb[i] != 1 && a[i] > max)
-        {
-            max = a[i];
-        }
-    }
-
-    get_distances(points, input_count, 1);
-
-    printf("(A): %i\n", max);
-
-    int totals[cz];
-
-    for(int i = 0; i < cz; i++)
-    {
-        int total = 0;
-
-        for(int j = 0; j < input_count; j++)
-        {
-           total += points[j]->distances[i]->dist;
-        }
-
-        totals[i] = total;
-    }
-
-    int m = 0;
-
-    int thing = 0;
-
-    for(int i = 0; i < cz; i++)
-    {
-        if(totals[i] < 10000)
-        {
-            //printf("%i\n", m);
-            thing++;
-        }
-    }
-
-    printf("(B): %i\n", thing);
-}
-
-struct Point * get_point(char * line, int input_count)
-{
-    int i = 0;
-
-    char * q;
-    char * co[2];
-
-    struct Point * point = malloc(sizeof(struct Point));
-
-    line[strlen(line) - 1] = '\0';
-
-    q = strtok(line, ", ");
-
-    while(q != NULL)
-    {
-        co[i++] = q;
-        q = strtok(NULL, ", ");
-    }
-
-    point->x = atoi(co[0]);
-    point->y = atoi(co[1]);
-    point->id = input_count;
-    point->d_count = 0;
-
-    return point;
-}
-
-int md(int x1, int y1, int x2, int y2)
-{
-     return abs(x1 - x2) + abs(y1 - y2);
-}
